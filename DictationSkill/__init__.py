5132ddd521dbd1845e6a3791b0258c82ecf41b75
JarbasAI <somom@30wave.com>
2017-03-05 12:12:51 -0600

    Add files via upload

---
+++ DictationSkill/__init__.py
@@ -0,0 +1,81 @@
+# Copyright 2016 Mycroft AI, Inc.
+#
+# This file is part of Mycroft Core.
+#
+# Mycroft Core is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Mycroft Core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+import time
+import os
+
+__author__ = 'jarbas'
+
+logger = getLogger(__name__)
+
+
+class DictationSkill(MycroftSkill):
+
+    def __init__(self):
+        super(DictationSkill, self).__init__(name="DictationSkill")
+        self.dictating = False
+        self.words = ""
+        self.path = "/home/user/jarbas-core/mycroft/skills/DictationSkill/dictations"
+        self.path = os.path.dirname(__file__) + "/dictations"
+        # check if folders exist
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+
+    def initialize(self):
+
+        start_dict_intent = IntentBuilder("StartDictationIntent")\
+            .require("StartDictationKeyword").build()
+
+        self.register_intent(start_dict_intent,
+                             self.handle_start_dict_intent)
+
+    def handle_start_dict_intent(self, message):
+        self.dictating = True
+        self.speak("Dictation Mode Started")
+
+    def save(self):
+        # save
+        print self.words
+        #TODO let user set savefile name
+        path = self.path + "/" + str(time.time()) + ".txt"
+        wfile = open(path, "w")
+        wfile.write(self.words)
+        wfile.close()
+
+    def stop(self):
+        pass
+
+    def Converse(self, transcript, lang="en-us"):
+        if self.dictating:
+            #TODO better stop handling, using keyword.voc
+            if ("stop dictating" or "stop dictation" or "dictating stop" or "dictation stop") in transcript[0]:
+                self.dictating= False
+                self.save()
+                self.speak("Dictation Mode Stopped")
+            else:
+                self.words += (transcript[0]) + "\n"
+            return True
+        else:
+            return False
+
+
+def create_skill():
+    return DictationSkill()
