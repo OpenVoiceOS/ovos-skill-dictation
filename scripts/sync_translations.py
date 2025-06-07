859c659fa6d5a170ddf5cddf6a158929694571d9
miro <jarbasai@mailfence.com>
2025-06-07 18:59:22 +0100

    fix: automations

--- scripts/sync_translations.py
+++ scripts/sync_translations.py
@@ -5,10 +5,11 @@ TODO - before PR merge
 import json
 from os.path import dirname
 import os
+from ovos_utils.bracket_expansion import expand_template
+from ovos_utils.list_utils import flatten_list, deduplicate_list
 
 locale = f"{dirname(dirname(__file__))}/locale"
 tx = f"{dirname(dirname(__file__))}/translations"
-
 
 for lang in os.listdir(tx):
     intents = f"{tx}/{lang}/intents.json"
@@ -16,58 +17,64 @@ for lang in os.listdir(tx):
     vocs = f"{tx}/{lang}/vocabs.json"
     regexes = f"{tx}/{lang}/regexes.json"
 
+    os.makedirs(f"{locale}/{lang}", exist_ok=True)
+
     if os.path.isfile(intents):
         with open(intents) as f:
             data = json.load(f)
         for fid, samples in data.items():
             if samples:
-                samples = list(set([s.strip() for s in samples
-                           if s and s.strip() != "[UNUSED]"]))  # s may be None
+                samples = deduplicate_list(flatten_list([expand_template(s.strip())
+                                                         for s in samples
+                                                         if s and s.strip() != "[UNUSED]"]))  # s may be None
                 if fid.startswith("/"):
                     p = f"{locale}/{lang.lower()}{fid}"
                 else:
                     p = f"{locale}/{lang.lower()}/{fid}"
-                os.makedirs(os.path.dirname(p), exist_ok=True)
-                with open(f"{locale}/{lang.lower()}/{fid}", "w") as f:
-                    f.write("\n".join(sorted(samples)))
+                os.makedirs(dirname(p), exist_ok=True)
+                with open(p, "w") as f:
+                    f.write("\n".join(sorted([s.replace("  ", " ") for s in samples])))
 
     if os.path.isfile(dialogs):
         with open(dialogs) as f:
             data = json.load(f)
         for fid, samples in data.items():
             if samples:
-                samples = list(set([s.strip() for s in samples
-                           if s and s.strip() != "[UNUSED]"]))  # s may be None
+                samples = deduplicate_list(flatten_list([expand_template(s.strip())
+                                                         for s in samples
+                                                         if s and s.strip() != "[UNUSED]"]))  # s may be None
                 if fid.startswith("/"):
                     p = f"{locale}/{lang.lower()}{fid}"
                 else:
                     p = f"{locale}/{lang.lower()}/{fid}"
                 os.makedirs(os.path.dirname(p), exist_ok=True)
                 with open(f"{locale}/{lang.lower()}/{fid}", "w") as f:
-                    f.write("\n".join(sorted(samples)))
+                    f.write("\n".join(sorted([s.replace("  ", " ") for s in samples])))
 
     if os.path.isfile(vocs):
         with open(vocs) as f:
             data = json.load(f)
         for fid, samples in data.items():
             if samples:
-                samples = list(set([s.strip() for s in samples
-                           if s and s.strip() != "[UNUSED]"]))  # s may be None
+                samples = deduplicate_list(flatten_list([expand_template(s.strip())
+                                                         for s in samples
+                                                         if s and s.strip() != "[UNUSED]"]))  # s may be None
                 if fid.startswith("/"):
                     p = f"{locale}/{lang.lower()}{fid}"
                 else:
                     p = f"{locale}/{lang.lower()}/{fid}"
                 os.makedirs(os.path.dirname(p), exist_ok=True)
                 with open(f"{locale}/{lang.lower()}/{fid}", "w") as f:
-                    f.write("\n".join(sorted(samples)))
+                    f.write("\n".join(sorted([s.replace("  ", " ") for s in samples])))
 
     if os.path.isfile(regexes):
         with open(regexes) as f:
             data = json.load(f)
         for fid, samples in data.items():
             if samples:
-                samples = list(set([s.strip() for s in samples
-                           if s and s.strip() != "[UNUSED]"]))  # s may be None
+                samples = deduplicate_list(flatten_list([expand_template(s.strip())
+                                                         for s in samples
+                                                         if s and s.strip() != "[UNUSED]"]))  # s may be None
                 if fid.startswith("/"):
                     p = f"{locale}/{lang.lower()}{fid}"
                 else:
